640711acb242d836b9fade6676c12e1998ab72ec
DimaTiho <dmitrotiho@gmail.com>
2025-05-11 00:35:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -374,7 +374,7 @@ async def finalize_order(call: types.CallbackQuery):
     analytics_sheet.update("B4", f"=SUM({sheet.title}!H2:H)")
     analytics_sheet.update("B5", f"=INDEX({sheet.title}!D2:D, MODE(MATCH({sheet.title}!D2:D, {sheet.title}!D2:D, 0)))")
 
-    await call.message.answer("✅ Замовлення прийнято! Очікуйте на дзвінок або SMS."reply_markup=kb)
+    await call.message.answer("✅ Замовлення прийнято! Очікуйте на дзвінок або SMS.",reply_markup=kb)
     user_data.pop(uid, None)
                               
 async def notify_sent_orders():
